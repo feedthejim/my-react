86bf62588423e02902dc5532b59b7ea37afb6bc1
Jimmy Lai <laijimmy0@gmail.com>
2018-07-28 18:07:18 +0200

    remove dead code

--- src/myReact.js
+++ src/myReact.js
@@ -58,20 +58,6 @@ const render = (element, parentDom) =>
     ),
   )(element);
 
-// function createElement(type, config, ...args) {
-//   const props = Object.assign({}, config);
-//   const hasChildren = args.length > 0;
-//   const rawChildren = hasChildren ? [].concat(...args) : [];
-//   props.children = rawChildren
-//     .filter(c => c != null && c !== false)
-//     .map(c => c instanceof Object ? c : createTextElement(c));
-//   return { type, props };
-// }
-
-// function createTextElement(value) {
-//   return createElement(TEXT_ELEMENT, { nodeValue: value });
-// }
-
 const createTextElement = value =>
   createElement(TEXT_DOCUMENT, { nodeValue: value });
 
